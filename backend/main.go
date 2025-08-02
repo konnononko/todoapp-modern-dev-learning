2cbb2f33018ece79c0a150c4f220003608e352fd
Yusuke Konno <konnononko@gmail.com>
2025-08-02 17:10:54 +0900

    refactor: ステータスコードを明示

--- backend/main.go
+++ backend/main.go
@@ -32,6 +32,7 @@ func main() {
 
 func getTodos(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(todos)
 }
 
