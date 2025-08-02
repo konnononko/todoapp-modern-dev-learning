130c1c42a305163450015204d3c0320fc20331d3
Yusuke Konno <konnononko@gmail.com>
2025-08-02 11:40:25 +0900

    /todos Get APIを実装

---
+++ backend/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
+var todos = []Todo{}
+var nextID = 1
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/todos", getTodos)
+
+	http.ListenAndServe(":8080", r)
+}
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(todos)
+}
